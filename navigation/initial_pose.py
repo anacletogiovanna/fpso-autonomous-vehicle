f30c9ea02457ae9cfe43a6f28fc3f7057321d50e
ElizCarvalho <elizabethcarvalh0@yahoo.com>
2021-03-18 00:23:33 -0300

    Update initial_pose.py

--- navigation/initial_pose.py
+++ navigation/initial_pose.py
@@ -4,14 +4,14 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point, Quaternion
 from tf.transformations import quaternion_from_euler
 
 def set_initial_pose(pose):
-    rospy.init_node('set_initial_pose', anonymous=False) #Nome do nó de controle
-    pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=10)
-    rate = rospy.Rate(1) #10Hz
-
-    #Criando o objeto para setar a pose inicial informada
+	rospy.init_node('set_initial_pose', anonymous=False) #Nome do nó de controle
+	pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=10)
+	rate = rospy.Rate(1) #10Hz
+	
+	#Criando o objeto para setar a pose inicial informada
 	quat = quaternion_from_euler(0, 0, pose['z'])
-    initpose_msg = PoseWithCovarianceStamped()
-    initpose_msg.header.frame_id = 'map'
+	initpose_msg = PoseWithCovarianceStamped()
+	initpose_msg.header.frame_id = 'map'
 	initpose_msg.header.stamp = rospy.Time.now()
 	initpose_msg.pose = Pose(Point(pose['x'], pose['y'], 0.000), Quaternion(quat[0], quat[1], quat[2], quat[3]))
 	
