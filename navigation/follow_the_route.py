182a0ce4de3ca11709397b6f4ab5f2035228bafb
ElizCarvalho <elizabethcarvalh0@yahoo.com>
2021-03-18 02:23:21 -0300

    Update follow_the_route.py

--- navigation/follow_the_route.py
+++ navigation/follow_the_route.py
@@ -3,29 +3,22 @@
 import yaml
 import rospy
 from time import sleep
-from std_msgs.msg import Bool
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 from move_base_msgs.msg import MoveBaseActionResult
 
-def listener_photo_callback(data):
-    global status_photo
-    status_photo = data
 	
 def listener_robot_callback(data):
     global status
     status = data.status.status
 	
 def set_goal(count, pos, quat):
-	
 	pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 	rate = rospy.Rate(1) #10Hz
-	
 	#Criando o objeto para setar a pose inicial informada
 	pose_msg = PoseStamped()
 	pose_msg.header.frame_id = 'map'
 	pose_msg.header.stamp = rospy.Time.now()
 	pose_msg.pose = Pose(Point(pos['x'], pos['y'], 0.000), Quaternion(quat['x'], quat['y'], quat['z'], quat['w']))
-	
 	if not rospy.is_shutdown():
 		rospy.loginfo("Deslocando para pose: %s", count)
 		pub.publish(pose_msg)
@@ -37,29 +30,19 @@ def set_goal(count, pos, quat):
 def main():
 	with open("fpso_poses.yaml", 'r') as stream:
 		poses = yaml.load(stream)
-	
 	rospy.init_node('fpso', anonymous=False) #No de controle
 	
 	while True:
-	
-		set_goal(0, poses[0]['position'], poses[0]['quaternion'])
-		
 		for i in range(len(poses)):
-		
-			#Verificando se ja cheguei no meu objetivo
-			while not rospy.is_shutdown():
-				rospy.Subscriber('/move_base/result', MoveBaseActionResult, listener_robot_callback)
-				#rospy.Subscriber('/take_picture', Bool, listener_photo_callback)
-				#if status_photo == True:
-				if status == 3:
-					break
-					
 			#Setando a pose objetivo da base movel
 			set_goal(i+1, poses[i+1]['position'], poses[i+1]['quaternion'])
 			sleep(5)
-			
-          
+			#Verificando se ja cheguei no meu objetivo
+			while not rospy.is_shutdown():
+				rospy.Subscriber('/move_base/result', MoveBaseActionResult, listener_robot_callback)
+				if status == 3:
+					break
+					
 if __name__ == '__main__':
 	status = 0
-	#status_photo = Bool()
 	main()
