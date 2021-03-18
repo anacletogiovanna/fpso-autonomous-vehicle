b10736ab3cf9f742ee9a94df11050607e93c6633
ElizCarvalho <elizabethcarvalh0@yahoo.com>
2021-03-18 00:54:24 -0300

    Update method get coord

--- navigation/initial_pose.py
+++ navigation/initial_pose.py
@@ -4,32 +4,32 @@ import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point, Quaternion
 from tf.transformations import quaternion_from_euler
 
-def set_initial_pose(pose):
+def set_initial_pose(pos, quat):
 	rospy.init_node('set_initial_pose', anonymous=False) #Nome do no de controle
 	pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=10)
 	rate = rospy.Rate(1) #10Hz
 	
 	#Criando o objeto para setar a pose inicial informada
-	quat = quaternion_from_euler(0, 0, pose['z'])
 	initpose_msg = PoseWithCovarianceStamped()
 	initpose_msg.header.frame_id = 'map'
 	initpose_msg.header.stamp = rospy.Time.now()
-	initpose_msg.pose.pose = Pose(Point(pose['x'], pose['y'], 0.000), Quaternion(quat[0], quat[1], quat[2], quat[3]))
+	initpose_msg.pose.pose = Pose(Point(pos['x'], pos['y'], 0.000), Quaternion(quat['x'], quat['y'], quat['z'], quat['w']))
 	
 	if not rospy.is_shutdown():
+		rospy.loginfo("Setando pose inicial da base movel.")
 		pub.publish(initpose_msg)
 		rate.sleep()
 		pub.publish(initpose_msg)
-		rospy.loginfo("Setando pose inicial da base movel.")
+		rospy.loginfo(initpose_msg)
 		rate.sleep()
     
 def main():
 	#Lendo a posicao e orientacao da pose inicial no arquivo 
-	with open("initial_pose.yaml", 'r') as stream:
-		initial_pose = yaml.load(stream)
+	with open("fpso_poses.yaml", 'r') as stream:
+		initial_pose = yaml.load(stream)[0]
 		
-	#Setando a pose inicial do robo
-	set_initial_pose(initial_pose[0]['pose'])
+	#Setando a pose inicial da base movel
+	set_initial_pose(initial_pose['position'], initial_pose['quaternion'])
           
 if __name__ == '__main__':
     main()
